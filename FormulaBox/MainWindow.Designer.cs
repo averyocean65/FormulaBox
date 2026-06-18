f0a74522b8daa3f8f9870406569fa7b48ee7b702
averyocean65 <averytoad65@gmail.com>
2026-06-18 12:36:21 +0200

    add buttons

--- FormulaBox/MainWindow.Designer.cs
+++ FormulaBox/MainWindow.Designer.cs
@@ -32,6 +32,8 @@
             textBox1 = new TextBox();
             label2 = new Label();
             listView1 = new ListView();
+            sampleButton = new Button();
+            calculateButton = new Button();
             SuspendLayout();
             // 
             // label1
@@ -48,7 +50,7 @@
             textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             textBox1.Location = new Point(12, 32);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(605, 27);
+            textBox1.Size = new Size(354, 27);
             textBox1.TabIndex = 1;
             // 
             // label2
@@ -66,15 +68,37 @@
             listView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             listView1.Location = new Point(12, 100);
             listView1.Name = "listView1";
-            listView1.Size = new Size(605, 308);
+            listView1.Size = new Size(472, 289);
             listView1.TabIndex = 3;
             listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // sampleButton
+            // 
+            sampleButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            sampleButton.Location = new Point(372, 31);
+            sampleButton.Name = "sampleButton";
+            sampleButton.Size = new Size(112, 29);
+            sampleButton.TabIndex = 4;
+            sampleButton.Text = "Sample";
+            sampleButton.UseVisualStyleBackColor = true;
+            // 
+            // calculateButton
+            // 
+            calculateButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            calculateButton.Location = new Point(12, 395);
+            calculateButton.Name = "calculateButton";
+            calculateButton.Size = new Size(472, 29);
+            calculateButton.TabIndex = 5;
+            calculateButton.Text = "Calculate";
+            calculateButton.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(629, 420);
+            ClientSize = new Size(496, 436);
+            Controls.Add(calculateButton);
+            Controls.Add(sampleButton);
             Controls.Add(listView1);
             Controls.Add(label2);
             Controls.Add(textBox1);
@@ -92,5 +116,7 @@
         private TextBox textBox1;
         private Label label2;
         private ListView listView1;
+        private Button sampleButton;
+        private Button calculateButton;
     }
 }
